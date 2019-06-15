fc6d92925141e734bf92215a44ffcfb8cff3c53e
MCardenas <mcarden001@citymail.cuny.edu>
2019-06-15 11:55:45 -0400

    deploy

---
+++ precache-manifest.d959e04f4751f009c5e6af5bf3f18229.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc305268ced099edad38ce9922da2f48",
+    "url": "/deploy-clicky/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/deploy-clicky/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "501e94f7384467f20368",
+    "url": "/deploy-clicky/static/js/main.2543aa32.chunk.js"
+  },
+  {
+    "revision": "1bf11085786980a564f1",
+    "url": "/deploy-clicky/static/js/runtime~main.fd5c6bef.js"
+  }
+]);
